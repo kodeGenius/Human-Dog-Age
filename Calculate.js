be39eb1d910305b320c9fd1058e83d07a2e92381
Project <oladosu777@gmail.com>
2018-12-20 12:57:02 +0100

    new

--- Calculate.js
+++ Calculate.js
@@ -1,9 +1,10 @@
 const age = () =>{
-	let myAge = document.getElementsById('age').value;
+	let myAge = document.getElementById("age").value;
 	let early = 2;
 	early *= 10.5;
 	let later = myAge-2;
 	later *= 4;
 	let dogAge = later + early;
-	document.getElementById('mini').innerHTML = `Since your age is ${myAge} years old, you will be ${dogAge} years old in Dog Age.`
+	document.getElementById("mini").innerHTML = `Since your age is ${myAge} years old, you will be ${dogAge} years old in Dog Age.`;
 }
+//document.getElementById("mini").innerHTML =
